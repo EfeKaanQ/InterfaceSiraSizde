b6aecde0d90a7831dc0cb53ae315bb1e660133fb
efe33 <efe33@BPC04>
2023-12-21 13:18:32 +0300

    Proje dosyası ekle.

---
+++ InterfaceSiraSizde/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceSiraSizde
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Kitap kitap = new Kitap { Ad = "Roman Havası Kitabı♥" };
+            Dergi dergi = new Dergi { Ad = "Gazete☻" };
+            Ansiklopedi ansiklopedi = new Ansiklopedi { Ad = "Ansiklopedi adı bilmiom☻" };
+
+            kitap.Oku();
+            dergi.Oku();
+            ansiklopedi.Oku();
+
+
+
+
+
+        }
+    }
+
+
+
+    interface IOkunabilir
+    {
+        void Oku();
+    }
+
+
+    public class Kitap : IOkunabilir
+    {
+        public string Ad { get; set; }
+
+        public void Oku()
+        {
+            Console.WriteLine("kitabı oku : " + Ad);
+        }
+    }
+
+
+    public class Dergi : IOkunabilir
+    {
+        public string Ad { get; set; }
+
+        public void Oku()
+        {
+            Console.WriteLine("dergiyi oku : " + Ad);
+        }
+    }
+
+
+    public class Ansiklopedi : IOkunabilir
+    {
+        public string Ad { get; set; }
+
+        public void Oku()
+        {
+            Console.WriteLine("ansiklopediyi oku : " + Ad);
+        }
+    }
+
+}  
+   
+
+
